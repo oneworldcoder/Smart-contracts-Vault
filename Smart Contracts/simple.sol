1fdb7bfa3f6e4bd42be41217e652b3fff57c45f1
oneworldcoder <52066039+oneworldcoder@users.noreply.github.com>
2020-10-15 13:47:09 +0530

    Added setMessage()

--- Smart Contracts/simple.sol
+++ Smart Contracts/simple.sol
@@ -6,3 +6,17 @@ contract Hello {
     }
     //Add a simple setMessage function here to change the message variable.
 }
+
+pragma solidity ^0.5.12;
+
+contract Hello {
+    string public message="Hello";
+    
+    
+    function setMessage(string memory newMessage) public {
+        message = newMessage;
+    
+    }
+    //Add a simple setMessage function here to change the message variable.
+    
+}
